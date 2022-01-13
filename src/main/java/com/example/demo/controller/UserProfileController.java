a402c7899167ae418eb8098a0a3cf807e8c9ec52
winterrosy <wish759@naver.com>
2022-01-13 23:18:33 +0900

    insert, upadate

--- src/main/java/com/example/demo/controller/UserProfileController.java
+++ src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.UserProfile;
@@ -28,7 +33,28 @@ public class UserProfileController {
 	@GetMapping("/user/{id}")
 	public UserProfile getUserProfile(@PathVariable("id")String id) {
 		return userMap.get(id);
-		
 	}
+	@GetMapping("/user/all")
+	public List<UserProfile> getUserProfileList() {
+		return new ArrayList<UserProfile>(userMap.values());
+	}
+	
+	//추가
+	@PutMapping("/user/{id}")
+	public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+		UserProfile userProfile = new UserProfile(id, name, phone, address);
+		userMap.put(id, userProfile);
+	}
+	
+	//수정
+	@PostMapping("/user/{id}")
+	public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+		UserProfile userProfile = userMap.get(id);
+		userProfile.setName(name);
+		userProfile.setPhone(phone);
+		userProfile.setAddress(address);		
+}
+	
+	
 	
 }
